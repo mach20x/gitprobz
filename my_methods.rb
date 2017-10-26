eba636a579ff675a575c04ce76384e052e1ea7a2
Nick Kautz <Nick.Kautz@gmail.com>
2017-10-26 10:33:40 -0400

    new methods file, incl fizzbuzz()

---
+++ my_methods.rb
@@ -0,0 +1,15 @@
+def add_nums(a, b)
+  return a + b
+end
+
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    return "fizzbuzz"
+  elsif num % 3 == 0
+    return "fizz"
+  elsif num % 5 == 0
+    return "buzz"
+  else
+    return num
+  end
+end
